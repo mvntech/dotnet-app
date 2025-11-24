6c0debd12539e49dc89e49ef5ac7624d8951ba12
mvntahax <syedamuntahapk@gmail.com>
2025-11-24 21:32:03 +0500

    created db context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using dotnet_app.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_app.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+        }
+        DbSet<Student>Students { get; set; }
+    }
+}
--- obj/Debug/net8.0/dotnet-app.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5942459c074ff02e36460340f0e0697b6a9983fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5941c1134021253a02945f364527a32eba796d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
