b5941c1134021253a02945f364527a32eba796d7
mvntahax <syedamuntahapk@gmail.com>
2025-11-24 21:26:56 +0500

    created a model class

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotnet_app.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public int Age { get; set; }
+    }
+}
